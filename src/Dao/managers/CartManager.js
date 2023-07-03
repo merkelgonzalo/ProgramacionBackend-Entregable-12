a488268aad7cff6a52cf128621601f20fc6e3956
Gonzalo Merkel <merkelgonzalo.ok@gmail.com>
2023-07-03 16:14:45 -0300

    Changing the CartManager's structure

--- src/Dao/managers/CartManager.js
+++ src/Dao/managers/CartManager.js
@@ -6,12 +6,15 @@ const managerAccess = new ManagerAccess();
 
 export default class ProductManager {
 
-    constructor() { }
+    constructor() {
+        this.model = cartModel;
+        this.productModel = productModel;
+    }
 
     addCart = async () => {
         try {
             await managerAccess.saveLog('POST a cart');
-            let result = await cartModel.create({});
+            let result = await this.model.create({});
             return result;
         } catch (error) {
             console.log('Cannot post the cart in manager with mongoose: ' + error)
@@ -22,13 +25,13 @@ export default class ProductManager {
         try {
             await managerAccess.saveLog('POST product in a cart');
             let result;
-            const cart = await cartModel.find({ _id: idCart });
-            const product = await productModel.find({ _id: idProduct });
+            const cart = await this.model.find({ _id: idCart });
+            const product = await this.productModel.find({ _id: idProduct });
             if (cart.length === 0 || product.length === 0) {
                 result = 0;
             } else {
                 cart[0].products.push({ product: idProduct, quantity: quantityBody });
-                result = await cartModel.updateOne({ _id: idCart }, { $set: cart[0] });
+                result = await this.model.updateOne({ _id: idCart }, { $set: cart[0] });
             }
             return result;
         } catch (error) {
@@ -39,7 +42,7 @@ export default class ProductManager {
     getCarts = async (req) => {
         try {
             await managerAccess.saveLog('GET all carts');
-            let carts = await cartModel.find();
+            let carts = await this.model.find();
             return carts;
         } catch (error) {
             console.log('Cannot get carts with mongoose in manager: ' + error)
@@ -49,7 +52,7 @@ export default class ProductManager {
     getCartById = async (cid) => {
         try {
             await managerAccess.saveLog('GET a cart');
-            const result = await cartModel.find({ _id: cid });
+            const result = await this.model.find({ _id: cid });
             return result;
         } catch (error) {
             console.log('Cannot get the cart in manager with mongoose: ' + error);
@@ -59,7 +62,7 @@ export default class ProductManager {
     getCartByIdPopulate = async (cid) => {
         try {
             await managerAccess.saveLog('GET a cart');
-            const result = await cartModel.findById(cid).populate("products.product").lean();
+            const result = await this.model.findById(cid).populate("products.product").lean();
             return result;
         } catch (error) {
             console.log('Cannot get the cart in manager with mongoose: ' + error);
@@ -69,13 +72,13 @@ export default class ProductManager {
     deleteCart = async (cid) => {
         try {
             await managerAccess.saveLog('DELETE all products in a cart');
-            const cart = await cartModel.find({ _id: cid });
+            const cart = await this.model.find({ _id: cid });
             let result;
             if (cart.length === 0) {
                 result = 0;
             }else{
                 cart[0].products = [];
-                result = await cartModel.updateOne({ _id: cid }, { $set: cart[0] });
+                result = await this.model.updateOne({ _id: cid }, { $set: cart[0] });
             }
             return result;
         } catch (error) {
@@ -87,8 +90,8 @@ export default class ProductManager {
         try {
             await managerAccess.saveLog('DELETE a product in a cart');
 
-            const cart = await cartModel.find({ _id: idCart });
-            const product = await productModel.find({ _id: idProduct });
+            const cart = await this.model.find({ _id: idCart });
+            const product = await this.productModel.find({ _id: idProduct });
             let result;
 
             if (cart.length === 0 || product.length === 0) {
@@ -96,7 +99,7 @@ export default class ProductManager {
             } else {
                 const products = cart[0].products.filter(element => element.product._id != idProduct);
                 cart[0].products = products;
-                result = await cartModel.updateOne({ _id: idCart }, { $set: cart[0] });
+                result = await this.model.updateOne({ _id: idCart }, { $set: cart[0] });
             }
             return result;
         } catch (error) {
@@ -107,14 +110,14 @@ export default class ProductManager {
     updateCart = async (cid, products) => {
         try {
             await managerAccess.saveLog('UPDATE all products in a cart');
-            const cart = await cartModel.findById(cid);
+            const cart = await this.model.findById(cid);
             console.log(cart)
             let result;
             if (!cart) {
                 result = 0;
             }else{
                 cart.products = products;
-                result = await cartModel.updateOne({ _id: cid }, { $set: cart });
+                result = await this.model.updateOne({ _id: cid }, { $set: cart });
             }
             return result;
         } catch (error) {
@@ -125,8 +128,8 @@ export default class ProductManager {
     updateProduct = async (cid, pid, quantity) => {
         try {
             await managerAccess.saveLog('UPDATE product s quantity in a cart');
-            const cart = await cartModel.find({ _id: cid });
-            const product = await productModel.find({ _id: pid });
+            const cart = await this.model.find({ _id: cid });
+            const product = await this.productModel.find({ _id: pid });
             let result;
             if (cart.length === 0 || product.length === 0) {
                 result = 0;
@@ -136,7 +139,7 @@ export default class ProductManager {
                         element.quantity = quantity;
                     }
                 });
-                result = await cartModel.updateOne({ _id: cid }, { $set: cart[0] });
+                result = await this.model.updateOne({ _id: cid }, { $set: cart[0] });
             }
             return result;
         } catch (error) {
